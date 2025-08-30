c7fb83f7fb3759fb27809c19a0a35111031b6af5
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2025-08-30 11:41:26 +0700

    menampilkan data soal1 done

--- soal1a.php
+++ soal1a.php
@@ -20,7 +20,42 @@ if (!isset($_POST['step'])) {
 elseif ($_POST['step'] == "1") {
     $rows = (int)$_POST['rows'];
     $cols = (int)$_POST['cols'];
+    ?>
+    <form method="post">
+        <input type="hidden" name="step" value="2">
+        <input type="hidden" name="rows" value="<?= $rows ?>">
+        <input type="hidden" name="cols" value="<?= $cols ?>">
 
-    echo "Jumlah Baris: $rows | Jumlah Kolom: $cols";
+        <table cellpadding="5" cellspacing="0">
+            <?php for ($i = 1; $i <= $rows; $i++): ?>
+                <tr>
+                    <?php for ($j = 1; $j <= $cols; $j++): ?>
+                        <td>
+                            <?= $i . "." . $j ?> :
+                            <input type="text" name="data[<?= $i ?>][<?= $j ?>]" required>
+                        </td>
+                    <?php endfor; ?>
+                </tr>
+            <?php endfor; ?>
+        </table>
+
+        <br>
+        <button type="submit">SUBMIT</button>
+    </form>
+    <?php
 }
+
+elseif ($_POST['step'] == "2") {
+    $data = $_POST['data'];
+    ?>
+    <div style="padding:10px; width:250px;">
+        <?php foreach ($data as $i => $row): ?>
+            <?php foreach ($row as $j => $val): ?>
+                <?= $i . "." . $j ?> : <?= htmlspecialchars($val) ?> <br>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
+    <?php
+}
+
 ?>
